39da3b61648e3f9a80b5b7697211526c070fe2f3
Patron333 <brayantigreros2021@gmail.com>
2026-02-21 23:34:59 -0500

    Delete lab_03 directory

--- lab_03/etl/transform.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-# Transform sales data
-def transform_sales_data(df_sales):
-
-    # Convertir a datetime
-    df_sales["sale_date"] = pd.to_datetime(df_sales["sale_date"], errors="coerce")
-    return df_sales
-
